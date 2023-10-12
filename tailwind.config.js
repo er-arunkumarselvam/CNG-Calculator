256c1b2fd305265ab49fd4e4283e751c900283b7
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2023-10-12 14:38:35 +0530

    design UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        skyblue:"#9cecfb",
+        blue:"#65c7f7",
+        darkblue:"#0052d4",
+      }
+    },
   },
   plugins: [],
 };
